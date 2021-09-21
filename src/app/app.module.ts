41cacdcac0d349476a15bcff023f6b418847b462
chousann <1769862201@qq.com>
2021-09-21 17:14:18 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+const appRoutes: Routes = [
+  // {
+  //   path: '', component: DakaComponent
+  // },
+  // {
+  //   path: 'detail', component: DetailComponent
+  // }
+];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatInputModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
